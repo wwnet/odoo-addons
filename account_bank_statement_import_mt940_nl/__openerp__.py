cebc9afbd24ef5150e3412e2bbf255c0ea029387
Alexis Yushin <alexis@ww.net>
2016-07-03 16:04:22 +0300

    Updated module description

--- account_bank_statement_import_mt940_nl/__openerp__.py
+++ account_bank_statement_import_mt940_nl/__openerp__.py
@@ -3,21 +3,26 @@
     'name': "account_bank_statement_import_mt940_nl",
 
     'summary': """
-	This module imports MT940 statements for Dutch banks
-	""",
+	MT940 bank statement import for Dutch banks.
+        """,
 
     'description': """
-	Module to import MT940 bank statements
-	======================================
-
-	This module allows you to import MT940 files in Odoo: they are parsed and stored in human readable format in
-	Accounting \ Bank and Cash \ Bank Statements.
+    This module allows you to import MT940 files in Odoo.
 
 	At the moment only supports ING NL, might work with other banks, but not yet tested.
-    """,
-    'author': "White Willow BV",
+	    """,
+
+    'author': "White Willow B.V.",
     'website': "http://ww.net",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
+    # for the full list
     'category': 'Accounting & Finance',
     'version': '0.1',
+    'installable': True,
+
+    # any module necessary for this one to work correctly
     'depends': ['account_bank_statement_import'],
+
 }
